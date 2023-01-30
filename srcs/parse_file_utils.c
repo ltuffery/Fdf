6585088439ab673698bb6f4c743299e0c8f40ab6
Leo Tuffery <ltuffery@1H4.42angouleme.fr>
2023-01-30 19:07:55 +0100

    up free

--- srcs/parse_file_utils.c
+++ srcs/parse_file_utils.c
@@ -6,23 +6,33 @@
 /*   By: ltuffery <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:02:39 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/01/29 16:04:55 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/01/30 18:48:13 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/get_next_line.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int	count_file_lines(char *file)
 {
-	int	i;
-	int	fd;
+	int		i;
+	int		fd;
+	char	*line;
 
-	i = 0;
+	i = 1;
 	fd = open(file, O_RDONLY);
-	while (get_next_line(fd) != NULL)
+	line = get_next_line(fd);
+	if (line == NULL)
+		return (0);
+	while (line != NULL)
+	{
+		free(line);
 		i++;
+		line = get_next_line(fd);
+	}
+	free(line);
 	close(fd);
 	return (i);
 }
